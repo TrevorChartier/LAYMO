dfd754fd5c8fc0f63c20693ea3d1cfbdbe32e8be
TrevorChartier <chartier.trevor@gmail.com>
2025-04-14 10:46:34 -0600

    Create CameraManager Class

---
+++ src/camera_manager.py
@@ -0,0 +1,33 @@
+""" 
+Class for configuring and capturing frames from PiCamera
+
+Usage:
+    camera = CameraManager()
+    frame = CameraManager.get_latest_frame()
+"""
+from picamera2 import Picamera2
+import numpy as np
+import time
+
+class CameraManager:
+    
+    def __init__(self):
+        self.__camera = Picamera2()
+        
+        # Could play around with other configs and maybe .align_configuration()
+        camera_config = self.__camera.create_preview_configuration(
+            main={"size": (640, 480), "format": "RGB888"}
+        )
+        self.__camera.configure(camera_config)
+        self.__camera.start()
+        
+    def get_latest_frame(self) -> np.ndarray:
+        """
+        Retrieve the most recent frame
+        
+        Returns: 
+            frame (np.ndarray): Copy of latest frame as a numpy array
+        """
+        return self.__camera.capture_array()
+
+                
