8687d3d1e477ad510800c6384e6fe1eb411553cb
TrevorChartier <chartier.trevor@gmail.com>
2025-04-16 20:00:48 -0600

    Add Documentation for Line Detector

--- src/line_detector.py
+++ src/line_detector.py
@@ -9,10 +9,18 @@ def calc_error(image: np.ndarray):
     pass
 
 
-def _crop_frame(image: np.ndarray) -> np.ndarray:
+def _crop(image: np.ndarray) -> np.ndarray:
     pass
 
-
+def __preprocess(image: np.ndarray) -> np.ndarray:
+    """
+    Performs necessary preprocessing steps on an image to improve contour detection
+    of blue line
+    """
+    ## Maybe convert image to HSV
+    ## Gaussian blur
+    ## Binary thresholding or canny edge detection to isolate line
+    
 def _get_line_center_x(image: np.ndarray) -> int:
     """"
     Computes the horizontal center (x-coordinate) of the bounding box that contains
@@ -27,4 +35,8 @@ def _get_line_center_x(image: np.ndarray) -> int:
         detected line in the image.
     
     """
+    
+    ## Use a contour finding algorithm (built into OpenCV)
+    ## Might have to filter contours to get the one that is the line
+    ## Return the horizontal center of the contour
     pass 
