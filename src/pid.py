1d08ec5cec0180efdf153ce189dd74a6395e8883
Trevor Chartier <chartier.trevor@gmail.com>
2025-04-23 11:17:14 -0600

    Implement calc_correction method

--- src/pid.py
+++ src/pid.py
@@ -1,21 +1,22 @@
 """This Module Implements the PID Class"""
 
+
 class PID:
     """"
     Class for Creating Proportional Integral Derivative Controllers
-    
+
     Usage:
         controller = PID(kp=1.0,ki=1.0,kd=1.0)
         correction = controller.calc_correction(error=4.2)
     """
-    
+
     def __init__(self, ki: float, kp: float, kd: float):
         self.__kp = kp
         self.__ki = ki
         self.__kd = kd
         self.__previous_error = 0.0
         self.__error_sum = 0.0
-    
+
     def calc_correction(self, error: float) -> float:
         """
         Computes the control output (correction) using the PID formula
@@ -29,3 +30,10 @@ class PID:
             float: The computed correction value based on the proportional, 
                 integral, and derivative terms.
         """
+        self.__error_sum += error
+        output = (self.__kp * error
+                  + self.__kd * (error - self.__previous_error)
+                  + self.__ki * self.__error_sum)
+        self.__previous_error = error
+
+        return output
