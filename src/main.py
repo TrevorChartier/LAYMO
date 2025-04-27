0aab996e151ac9b7f89dcf6bd4c2e7b748c7fe33
Trevor Chartier <chartier.trevor@gmail.com>
2025-04-27 12:46:29 -0600

    Add Dynamic Throttle Control

--- src/main.py
+++ src/main.py
@@ -14,15 +14,24 @@ from pid import PID
 THROTTLE_PIN = 0 # PCA9685 pin of ESC connection
 STEERING_PIN = 1 # PCA9685 pin of Servo Connection
 
-car = Car(steering_pin=STEERING_PIN, throttle_pin=THROTTLE_PIN)
+car = Car(steering_pin=STEERING_PIN, throttle_pin=THROTTLE_PIN, max_speed=0.5)
 
-# Signal Handlers -- Stop Car ff any Error or ^C
+# Signal Handlers -- Stop Car if any Error or ^C
 signal.signal(signal.SIGINT, car.stop)  
 signal.signal(signal.SIGTERM, car.stop) 
 
 camera = CameraManager()
 steering_controller = PID(kp=1, ki=0, kd=0)
+throttle_controller = PID(kp=3.75, ki=0, kd=0)
 time.sleep(1)
+
+def set_speed_manual(iteration):
+    """Method to manually pulse throttle"""
+    if (iteration//10) % 2 == 0:
+        car.set_speed(0.2)
+    else:
+        car.set_speed(0)
+
 
 RUN_TIME = 5 # Runtime in seconds
 NUM_ITERATIONS = 32 * RUN_TIME # Approx 32hz
@@ -30,16 +39,20 @@ NUM_ITERATIONS = 32 * RUN_TIME # Approx 32hz
 print("Beginning Control Loop")
 for i in range(NUM_ITERATIONS):
     frame = camera.get_latest_frame()
-    error = line_detector.calc_error(frame, roi=[0.1, 0.6])
-    if error is None:
-        print("No Line Detected")
-        continue
-    steering_control_output = steering_controller.calc_output(error)
+    steering_error = line_detector.calc_error(frame, roi=[0.1, 0.6])
+
+    steering_control_output = steering_controller.calc_output(steering_error)
     car.set_steering(steering_control_output)
 
-    if (i//10) % 2 == 0:
-        car.set_speed(0.2)
+    if steering_error is None and (abs(car.current_steering_pos()) < 0.75):
+        break # Reached the end of the line
+
+    # Adjust speed
+    throttle_error = abs(line_detector.calc_error(frame, roi=[0.3,1]))
+    if throttle_error is not None:
+        throttle_control_output = throttle_controller.calc_output(throttle_error)
+        car.set_speed(1/(throttle_control_output + 1))
     else:
-        car.set_speed(0)
+        set_speed_manual(i)
 
 car.stop()
