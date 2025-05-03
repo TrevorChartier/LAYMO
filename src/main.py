4eb994fa8003989faff2e79db9ad7e8e9f1557f5
TrevorChartier <chartier.trevor@gmail.com>
2025-05-02 19:15:34 -0600

    Allow Second Chance to Get Back On Line

--- src/main.py
+++ src/main.py
@@ -23,24 +23,25 @@ signal.signal(signal.SIGTERM, car.stop)
 signal.signal(signal.SIGHUP, car.stop)
 
 camera = CameraManager()
-steering_controller = PID(kp=1.5, ki=0, kd=0)
-throttle_controller = PID(kp=3.75, ki=0, kd=0)
+steering_controller = PID(kp=2.3, ki=0.08, kd=0)
 time.sleep(1)
 
 def is_throttled():
     """Checks for Undervoltages"""
     status = subprocess.run(['vcgencmd','get_throttled'], capture_output=True, text=True)
-    return status.stdout.strip() != 'throttled=0x0'
+    status  = status.stdout.strip()
+    return status == 'throttled=0x1' or status == 'throttled=0x4'
 
 
 def set_speed_manual(iteration):
     """Method to manually pulse throttle"""
-    if (iteration//10) % 2 == 0:
-        car.set_speed(0.2)
+    if (iteration//15) % 3 == 0:
+        car.set_speed(0.23)
     else:
-        car.set_speed(0)
+        car.set_speed(0.0)
 
-RUN_TIME = 5 # Runtime in seconds
+time_off_line = 0
+RUN_TIME = 25 # Runtime in seconds
 NUM_ITERATIONS = 32 * RUN_TIME # Approx 32hz
 print("Beginning Control Loop")
 for i in range(NUM_ITERATIONS):
@@ -48,16 +49,22 @@ for i in range(NUM_ITERATIONS):
         print("UNDERVOLTAGE DETECTED")
         car.stop() 
 
+    set_speed_manual(i)
     frame = camera.get_latest_frame()
-    steering_error = line_detector.calc_error(frame, roi=[0.2, 0.7])
-
-    steering_control_output = steering_controller.calc_output(steering_error)
-    car.set_steering(steering_control_output)
+    steering_error = line_detector.calc_error(frame, roi=[0.2, 0.8])
 
     if steering_error is None:
-        car.stop()
-        break # Reached the end of the line
-
-    set_speed_manual(i)
+        if abs(car.current_steering_pos()) > 0.7 and time_off_line < 45:
+            time_off_line += 1
+            continue # Likely went off line, try to recover    
+        else:
+            print(f"END OF LINE DETECTED")
+            car.stop()
+            break
+              
+    time_off_line = 0
+        
+    steering_control_output = steering_controller.calc_output(steering_error)
+    car.set_steering(steering_control_output)
 
 car.stop()
