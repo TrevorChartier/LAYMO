f6da1e892e0d41e2a5fe541f5f700e05d30cb491
Trevor Chartier <chartier.trevor@gmail.com>
2025-05-10 08:54:18 -0600

    Refactor Main

--- src/main.py
+++ src/main.py
@@ -10,61 +10,65 @@ import line_detector
 from car import Car
 from camera_manager import CameraManager
 from pid import PID
+from params import Params
 
 
-THROTTLE_PIN = 0 # PCA9685 pin of ESC connection
-STEERING_PIN = 1 # PCA9685 pin of Servo Connection
-
-car = Car(steering_pin=STEERING_PIN, throttle_pin=THROTTLE_PIN)
-
-# Signal Handlers -- Stop Car if any Error or ^C
-signal.signal(signal.SIGINT, car.stop)  
-signal.signal(signal.SIGTERM, car.stop) 
-signal.signal(signal.SIGHUP, car.stop)
-
+car = Car(steering_pin=Params.STEERING_PIN, throttle_pin=Params.THROTTLE_PIN)
 camera = CameraManager()
-steering_controller = PID(kp=2.3, ki=0.08, kd=0)
-time.sleep(1)
+steering_controller = PID(
+    kp=Params.KP_STEER, ki=Params.KI_STEER, kd=Params.KD_STEER)
+
+
+def handle_exit(signum, frame):
+    """ Signal Handlers """
+    car.stop()
+
+
+for sig in (signal.SIGINT, signal.SIGTERM, signal.SIGHUP):
+    signal.signal(sig, handle_exit)
+
 
 def is_throttled():
     """Checks for Undervoltages"""
-    status = subprocess.run(['vcgencmd','get_throttled'], capture_output=True, text=True)
-    status  = status.stdout.strip()
-    return status == 'throttled=0x1' or status == 'throttled=0x4'
+    status = subprocess.run(['vcgencmd', 'get_throttled'],
+                            capture_output=True, text=True, check=True)
+    status = status.stdout.strip()
+    return status in ['throttled=0x1', 'throttled=0x4']
 
 
-def set_speed_manual(iteration):
+def set_speed_manual(i):
     """Method to manually pulse throttle"""
-    if (iteration//15) % 3 == 0:
-        car.set_speed(0.23)
-    else:
-        car.set_speed(0.0)
+    car.set_speed(0.23 if (i // 15) % 3 == 0 else 0.0)
 
-time_off_line = 0
-RUN_TIME = 25 # Runtime in seconds
-NUM_ITERATIONS = 32 * RUN_TIME # Approx 32hz
-print("Beginning Control Loop")
-for i in range(NUM_ITERATIONS):
-    if is_throttled():
-        print("UNDERVOLTAGE DETECTED")
-        car.stop() 
 
-    set_speed_manual(i)
-    frame = camera.get_latest_frame()
-    steering_error = line_detector.calc_error(frame, roi=[0.2, 0.8])
+def control_loop():
+    time_off_line = 0
+    for i in range(Params.NUM_ITERATIONS):
+        if is_throttled():
+            print("UNDERVOLTAGE DETECTED")
+            car.stop()
 
-    if steering_error is None:
-        if abs(car.current_steering_pos()) > 0.7 and time_off_line < 45:
-            time_off_line += 1
-            continue # Likely went off line, try to recover    
-        else:
-            print(f"END OF LINE DETECTED")
+        set_speed_manual(i)
+        frame = camera.get_latest_frame()
+        steering_error = line_detector.calc_error(frame, roi=[0.2, 0.8])
+
+        if steering_error is None:
+            if (abs(car.current_steering_pos()) > Params.STEERING_THRESHOLD
+                    and time_off_line < Params.TIME_OFF_LINE_LIMIT):
+                time_off_line += 1
+                continue  # Likely went off line, try to recover
+            print("END OF LINE DETECTED")
             car.stop()
             break
-              
-    time_off_line = 0
-        
-    steering_control_output = steering_controller.calc_output(steering_error)
-    car.set_steering(steering_control_output)
 
-car.stop()
+        time_off_line = 0
+
+        output = steering_controller.calc_output(steering_error)
+        car.set_steering(output)
+    car.stop()
+
+
+if __name__ == "__main__":
+    print("Beginning Control Loop")
+    time.sleep(1)
+    control_loop()
