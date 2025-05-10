ae529660404994f6f1586f5242f29a9f35477203
TrevorChartier <chartier.trevor@gmail.com>
2025-05-10 15:08:56 -0600

    Use visualize script

--- scripts/capture_pipeline.py
+++ scripts/capture_pipeline.py
@@ -6,37 +6,23 @@ import cv2
 from laymo.camera_manager import CameraManager
 from laymo.line_detector import calc_error, preprocess
 from laymo.params import Params
+from laymo.pid import PID
+from laymo.visualize import visualize
 
 cam = CameraManager()
-img = cam.get_latest_frame()
-filepath = "data"
+steering_controller = PID(
+    kp=Params.KP_STEER, ki=Params.KI_STEER, kd=Params.KD_STEER)
+
+frame = cam.get_latest_frame()
+filepath = "data/test_img"
 roi = Params.ROI_STEER
 
-start = time.time()
-err = calc_error(img=img, roi=roi)
-print("Processing time: ", np.round(time.time()-start, 6))
-print("Error: ", err)
+err = calc_error(img=frame, roi=roi)
 
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.imshow(img_rgb)
-# Horizontal Lines Representing Cropped Region
-plt.axhline(y=img.shape[0] - roi[0]*img.shape[0], color='green')
-plt.axhline(y=img.shape[0] - roi[1]*img.shape[0], color='green')
+output = steering_controller.calc_output(err)
 
-if err is not None:
-    # Recalculate line center from error value
-    line_center = err * (img.shape[1] // 2) + img.shape[1]//2
-    plt.axvline(x=np.mean(line_center), color='red',
-                linewidth=2)  # Red Line at Detected Line Center  
-else:
-    print("No Line")
+img = visualize(frame, err, output)
+cv2.imwrite(f'{filepath}/detected_line.png', img)
 
-plt.savefig(f'{filepath}/detected_line.png')
-
-# Write binary image after processing to file
-processed_img = preprocess(img) 
-cv2.imwrite(f'{filepath}/processed_image.png', processed_img)
-
-# Write blue channel of image to file
-blue_channel = img[:, :, 0]
+blue_channel = frame[:, :, 0]
 cv2.imwrite(f'{filepath}/blue_channel_image.png', blue_channel)
